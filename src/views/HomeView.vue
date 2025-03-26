8881cbbb8d3f983fe488e0349376d21eb882e703
Vladimir Milosavljevic <vladimir.milosavljevic.21@singimail.rs>
2025-03-26 15:55:45 +0100

    Finished flight cards

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import type { FlightModel } from '@/models/flight.model';
 import { FlightService } from '@/services/flight.service';
-import axios from 'axios';
+import { formatTime } from '@/utils';
 import { ref } from 'vue';
 
 const flights = ref<FlightModel[]>()
@@ -10,8 +10,39 @@ FlightService.getFlights()
 </script>
 
 <template>
-    <ul>
-        <li v-for="f of flights" :key="f.id">{{ f.destination }} {{ f.flightNumber }} {{ f.plane }}</li>
-    </ul>
+    <div class="wrapper mb-3 mt-3">
+        <div class="card" style="width: 18rem;" v-for="f of flights" :key="f.id">
+            <img :src="`https://img.pequla.com/destination/${f.destination.toLowerCase().split(' ')[0]}.jpg`"
+                class="card-img-top" :alt="f.destination">
+            <div class="card-body">
+                <h5 class="card-title">{{ f.destination }}</h5>
+                <h6 class="card-subtitle mb-2 text-body-secondary">{{ f.flightNumber }}</h6>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item"><i class="fa-solid fa-clock-rotate-left"></i> {{ formatTime(f.estimatedAt ?? f.scheduledAt) }}
+                </li>
+                <li class="list-group-item">
+                    <i class="fa-solid fa-plane"></i> Plane: {{ f.plane }}
+                </li>
+                <li class="list-group-item">
+                    <i class="fa-solid fa-circle-info"></i> {{ f.gate ? `Gate: ${f.gate}/ Terminal: ${f.terminal}` : 'Terminal: '+ f.terminal }}
+                </li>
+            </ul>
+            <div class="card-body">
+                <RouterLink :to="`/flight/${f.id}`" class="btn btn-sm btn-primary"><i class="fa-solid fa-arrow-up-right-from-square"></i>Details
+                </RouterLink>
+            </div>
+        </div>
+    </div>
 </template>
+
+<style>
+.wrapper {
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+</style>
  
